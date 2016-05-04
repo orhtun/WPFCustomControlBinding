646548cf50ecdbd5893c9f9a0c876df06b8686a2
orhtun <orhtun@users.noreply.github.com>
2016-05-04 19:07:16 +0300

    initial commit

---
+++ WPFCustomControlBinding/CustomControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFCustomControlBinding
+{
+    /// <summary>
+    /// Interaction logic for CustomControl.xaml
+    /// </summary>
+    public partial class CustomControl : UserControl
+    {
+        public IEnumerable CustomItems
+        {
+            get { return (IEnumerable)GetValue(CustomItemsProperty); }
+            set { SetValue(CustomItemsProperty, value); }
+        }
+
+        public static readonly DependencyProperty CustomItemsProperty =
+             DependencyProperty.Register("CustomItems", typeof(IEnumerable), typeof(CustomControl), new PropertyMetadata(default(IEnumerable)));
+
+        public CustomControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ WPFCustomControlBinding/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.ObjectModel;
+using System.Windows;
+
+namespace WPFCustomControlBinding
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> DataStringList
+        {
+            get { return (ObservableCollection<string>)GetValue(DataStringListProperty); }
+            set { SetValue(DataStringListProperty, value); }
+        }
+
+        public static readonly DependencyProperty DataStringListProperty =
+             DependencyProperty.Register("DataStringList", typeof(ObservableCollection<string>), typeof(CustomControl), new PropertyMetadata(default(ObservableCollection<string>)));
+
+        public MainWindow()
+        {
+            DataStringList = new ObservableCollection<string>();
+            this.DataContext = this;
+
+            for (int i = 0; i < 10; i++)
+            {
+                DataStringList.Add(i.ToString());
+            }
+
+            InitializeComponent();
+        }
+    }
+}
